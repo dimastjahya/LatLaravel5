3893ad85aa4274f77c2826e7d71852e050e4dae5
dimas tjahya <dimas.tjahya@indomog.com>
2015-09-25 16:18:37 +0700

    yang kedua

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,6 +2,7 @@
 
 @section('konten')
         <div class="container">
+        <h2>nama peserta</h2>
             <div class="content">
                 <div class="title">{{$data['nama']}}</div>
                 <div>{{$data['email']}}</div>
